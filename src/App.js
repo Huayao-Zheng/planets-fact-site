01ab7e5a2fafcd656b314342c17b3c9ddbcb9920
lonelybuddy <henryzheng92@gmail.com>
2021-12-27 22:20:26 -0600

    added menu for mobile version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 // import data from './data.json';
 import Mercury from './pages/Mercury';
 
 const App = () => {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={<Mercury />}></Route>
-                <Route path="mercury" element={<Mercury />}></Route>
-            </Routes>
-        </>
+        <Routes>
+            <Route path="/*" element={<Navigate replace to="/mercury" />} />
+            <Route path="/mercury" element={<Mercury />} />
+            <Route path="/venus" element={<Mercury />} />
+        </Routes>
     );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navbar.style.css';
 import { Link } from 'react-router-dom';
+import planets from '../data.json';
 
 const Navbar = () => {
     return (
@@ -10,31 +11,18 @@ const Navbar = () => {
             </Link>
 
             <ul className="nav-bar">
-                <li>
-                    <Link to="/">mercury</Link>
-                </li>
-                <li>
-                    <Link to="venus">venus</Link>
-                </li>
-                <li>
-                    <Link to="earth">earth</Link>
-                </li>
-                <li>
-                    <Link to="mars">mars</Link>
-                </li>
-                <li>
-                    <Link to="jupiter">jupiter</Link>
-                </li>
-                <li>
-                    <Link to="saturn">saturn</Link>
-                </li>
-                <li>
-                    <Link to="uranus">uranus</Link>
-                </li>
-                <li>
-                    <Link to="neptune">neptune</Link>
-                </li>
+                {planets.map((planet, idx) => (
+                    <li key={idx}>
+                        <Link to={`/${planet.name.toLowerCase()}`}>{planet.name}</Link>
+                    </li>
+                ))}
             </ul>
+
+            <svg className="hamburger" xmlns="http://www.w3.org/2000/svg" width="24" height="17">
+                <g fill="#FFF" fillRule="evenodd">
+                    <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+                </g>
+            </svg>
         </header>
     );
 };
